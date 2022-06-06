136340cc0e80aadcaaa16c445dd5e2d6c1040810
Josephsan-2011 <83101904+Josephsan-2011@users.noreply.github.com>
2022-06-06 09:03:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+
 function draw() {
     
 }
 function setup() {
+    video=createCapture(VIDEO)
+    video.size(350,350)
     canvas=createCanvas(350,350)
+    posenet=ml5.poseNet(video,model_loaded)
+    posenet.on('pose',got_poses)
+}
+function model_loaded() {
+    console.log("Posenet is loaded")
+}
+function got_poses(results) {
+    if (results.length>0) {
+        console.log(results)
+    }
 }
